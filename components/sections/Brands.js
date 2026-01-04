ae29b9b64c11bd37d798b92614c929d242f76579
sagarpal04 <sagarpal0402@gmail.com>
2026-01-04 20:11:36 +0530

    added

--- components/sections/Brands.js
+++ components/sections/Brands.js
@@ -90,10 +90,10 @@ export default function Brands() {
             {/* Marquee Container */}
             <div className="relative">
                 {/* Left Fade */}
-                <div className="absolute left-0 top-0 bottom-0 w-32 bg-gradient-to-r from-white to-transparent z-10 pointer-events-none" />
+                <div className="absolute left-0 top-0 bottom-0 w-8 md:w-32 bg-gradient-to-r from-white to-transparent z-10 pointer-events-none" />
 
                 {/* Right Fade */}
-                <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-l from-white to-transparent z-10 pointer-events-none" />
+                <div className="absolute right-0 top-0 bottom-0 w-8 md:w-32 bg-gradient-to-l from-white to-transparent z-10 pointer-events-none" />
 
                 {/* Scrolling Content */}
                 <div className="flex whitespace-nowrap">
